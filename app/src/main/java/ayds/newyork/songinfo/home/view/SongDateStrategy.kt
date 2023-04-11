59785a7bacc0e7c4f251a022c715e553d971d9b1
AgusF22 <agus_p3@hotmail.com>
2023-04-11 17:02:02 -0300

    Implementada clase MonthSongDateStrategy

--- app/src/main/java/ayds/newyork/songinfo/home/view/SongDateStrategy.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/view/SongDateStrategy.kt
@@ -17,7 +17,23 @@ internal class DaySongDateStrategy: SongDateStrategy {
 internal class MonthSongDateStrategy: SongDateStrategy {
 
     override fun getSongDate(releaseDate: String): String {
-        TODO("Not yet implemented")
+        val monthAndYear = releaseDate.split("-")
+        val monthName = when (monthAndYear[1].toInt()) {
+            1 -> "January"
+            2 -> "February"
+            3 -> "March"
+            4 -> "April"
+            5 -> "May"
+            6 -> "June"
+            7 -> "July"
+            8 -> "August"
+            9 -> "September"
+            10 -> "October"
+            11 -> "November"
+            12 -> "December"
+            else -> ""
+        }
+        return "$monthName, ${monthAndYear[0]}"
     }
 
 }
