098c2547caa9d53ae5db18c05042aba8ad839374
Erik Kovach <78232947+CoachVach@users.noreply.github.com>
2023-04-11 17:41:28 -0300

    YearSongDateStrategy translated to english

--- app/src/main/java/ayds/newyork/songinfo/home/view/SongDateStrategy.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/view/SongDateStrategy.kt
@@ -46,20 +46,20 @@ internal class MonthSongDateStrategy: SongDateStrategy {
 internal class YearSongDateStrategy: SongDateStrategy {
 
     override fun getSongDate(releaseDate: String): String {
-        val anio = releaseDate.split("-")[0].toInt()
-        val bisiesto = esBisiesto(anio)
+        val year = releaseDate.split("-")[0].toInt()
+        val leap = isLeap(year)
 
-        val result = if (bisiesto) {
-            "$anio (leap year)"
+        val result = if (leap) {
+            "$year (leap year)"
         } else {
-            "$anio (not a leap year)"
+            "$year (not a leap year)"
         }
 
         return result
     }
 
-    private fun esBisiesto(anio: Int): Boolean {
-        return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)
+    private fun isLeap(year: Int): Boolean {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
     }
 
 }
