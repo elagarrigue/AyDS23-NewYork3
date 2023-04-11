8a2a7e03822652db499d879f5d5e5f97f863e328
nicolastradivarius <gonzaleznicolas574@gmail.com>
2023-04-11 17:09:47 -0300

    Factory pattern implemented for getting concrete strategy on demand

--- app/src/main/java/ayds/newyork/songinfo/home/view/SongDateStrategyFactory.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/view/SongDateStrategyFactory.kt
@@ -2,8 +2,12 @@ package ayds.newyork.songinfo.home.view
 
 object SongDateStrategyFactory {
 
-    fun get(releaseDatePrecision: String): SongDateStrategy {
-
+    fun get(releaseDatePrecision: String) {
+        when (releaseDatePrecision) {
+            "day" -> DaySongDateStrategy()
+            "month" -> MonthSongDateStrategy()
+            "year" -> YearSongDateStrategy()
+        }
     }
 
 }
