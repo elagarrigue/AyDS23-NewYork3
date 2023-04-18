8f54e1d8372d19b62e84361d2493b845ee7b9ed1
nicolastradivarius <gonzaleznicolas574@gmail.com>
2023-04-17 22:02:45 -0300

    Moved `testDB` method to the end of DataBase.java

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.java
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.java
@@ -13,52 +13,7 @@ import java.util.List;
 
 public class DataBase extends SQLiteOpenHelper {
 
-  public static void testDB()
-  {
 
-    Connection connection = null;
-    try
-    {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
-
-      //statement.executeUpdate("drop table if exists person");
-      //statement.executeUpdate("create table person (id integer, name string)");
-      //statement.executeUpdate("insert into person values(1, 'leo')");
-      //statement.executeUpdate("insert into person values(2, 'yui')");
-      ResultSet rs = statement.executeQuery("select * from artists");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("id = " + rs.getInt("id"));
-        System.out.println("artist = " + rs.getString("artist"));
-        System.out.println("info = " + rs.getString("info"));
-        System.out.println("source = " + rs.getString("source"));
-
-      }
-    }
-    catch(SQLException e)
-    {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
-    }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        // connection close failed.
-        System.err.println(e);
-      }
-    }
-  }
 
   public static void saveArtist(DataBase dbHelper, String artist, String info)
   {
@@ -132,5 +87,52 @@ public class DataBase extends SQLiteOpenHelper {
   @Override
   public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+  }
+
+  public static void testDB()
+  {
+
+    Connection connection = null;
+    try
+    {
+      // create a database connection
+      connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db");
+      Statement statement = connection.createStatement();
+      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+      //statement.executeUpdate("drop table if exists person");
+      //statement.executeUpdate("create table person (id integer, name string)");
+      //statement.executeUpdate("insert into person values(1, 'leo')");
+      //statement.executeUpdate("insert into person values(2, 'yui')");
+      ResultSet rs = statement.executeQuery("select * from artists");
+      while(rs.next())
+      {
+        // read the result set
+        System.out.println("id = " + rs.getInt("id"));
+        System.out.println("artist = " + rs.getString("artist"));
+        System.out.println("info = " + rs.getString("info"));
+        System.out.println("source = " + rs.getString("source"));
+
+      }
+    }
+    catch(SQLException e)
+    {
+      // if the error message is "out of memory",
+      // it probably means no database file is found
+      System.err.println(e.getMessage());
+    }
+    finally
+    {
+      try
+      {
+        if(connection != null)
+          connection.close();
+      }
+      catch(SQLException e)
+      {
+        // connection close failed.
+        System.err.println(e);
+      }
+    }
   }
 }
