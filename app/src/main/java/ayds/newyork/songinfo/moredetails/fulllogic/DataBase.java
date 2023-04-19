8af4a55b26792a106786b5e2b5449fd961261137
nicolastradivarius <gonzaleznicolas574@gmail.com>
2023-04-19 09:43:03 -0300

    Deleted useless comments

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.java
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.java
@@ -17,16 +17,13 @@ public class DataBase extends SQLiteOpenHelper {
 
   public static void saveArtist(DataBase dbHelper, String artist, String info)
   {
-    // Gets the data repository in write mode
     SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-// Create a new map of values, where column names are the keys
     ContentValues values = new ContentValues();
     values.put("artist", artist);
     values.put("info", info);
     values.put("source", 1);
 
-// Insert the new row, returning the primary key value of the new row
     long newRowId = db.insert("artists", null, values);
   }
 
@@ -35,29 +32,25 @@ public class DataBase extends SQLiteOpenHelper {
 
     SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
     String[] projection = {
             "id",
             "artist",
             "info"
     };
 
-// Filter results WHERE "title" = 'My Title'
     String selection = "artist  = ?";
     String[] selectionArgs = { artist };
 
-// How you want the results sorted in the resulting Cursor
     String sortOrder = "artist DESC";
 
     Cursor cursor = db.query(
-            "artists",   // The table to query
-            projection,             // The array of columns to return (pass null to get all)
-            selection,              // The columns for the WHERE clause
-            selectionArgs,          // The values for the WHERE clause
-            null,                   // don't group the rows
-            null,                   // don't filter by row groups
-            sortOrder               // The sort order
+            "artists",
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
     );
 
     List<String> items = new ArrayList<String>();
@@ -130,7 +123,6 @@ public class DataBase extends SQLiteOpenHelper {
       }
       catch(SQLException e)
       {
-        // connection close failed.
         System.err.println(e);
       }
     }
