ff2bc14420633b2f65255fc777618a97e99db98f
AgusF22 <agus_p3@hotmail.com>
2023-04-21 19:53:24 -0300

    converted OtherInfoWindow to Kotlin

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -1,119 +1,108 @@
-package ayds.newyork.songinfo.moredetails.fulllogic;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import ayds.newyork.songinfo.R;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.squareup.picasso.Picasso;
-import java.io.IOException;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+package ayds.newyork.songinfo.moredetails.fulllogic
 
-public class OtherInfoWindow extends AppCompatActivity {
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ayds.newyork.songinfo.R
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import com.squareup.picasso.Picasso
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.io.IOException
+import java.util.*
 
-  public final static String ARTIST_NAME_EXTRA = "artistName";
+class OtherInfoWindow : AppCompatActivity() {
+    private var textMoreDetails: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_info)
+        textMoreDetails = findViewById(R.id.textMoreDetails)
+        open(intent.getStringExtra("artistName"))
+    }
 
-  private TextView textMoreDetails;
-
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_other_info);
-    textMoreDetails = findViewById(R.id.textMoreDetails);
-    open(getIntent().getStringExtra("artistName"));
-  }
-
-  public void getArtistInfo(String artistName) {
-    Retrofit retrofit = new Retrofit.Builder()
+    fun getArtistInfo(artistName: String?) {
+        val retrofit = Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/svc/search/v2/")
             .addConverterFactory(ScalarsConverterFactory.create())
-            .build();
-    NYTimesAPI NYTimesAPI = retrofit.create(NYTimesAPI.class);
-    Log.e("TAG","artistName " + artistName);
-
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        String text = DataBase.getInfo(dataBase, artistName);
-        if (text != null) { // exists in db
-          text = "[*]" + text;
-        } else { // get from service
-          Response<String> callResponse;
-          try {
-            callResponse = NYTimesAPI.getArtistInfo(artistName).execute();
-            Log.e("TAG","JSON " + callResponse.body());
-            Gson gson = new Gson();
-            JsonObject jobj = gson.fromJson(callResponse.body(), JsonObject.class);
-            JsonObject response = jobj.get("response").getAsJsonObject();
-            JsonElement _abstract = response.get("docs").getAsJsonArray().get(0).getAsJsonObject().get("abstract");
-            JsonElement url = response.get("docs").getAsJsonArray().get(0).getAsJsonObject().get("web_url");
-            if (_abstract == null) {
-              text = "No Results";
-            } else {
-                text = _abstract.getAsString().replace("\\n", "\n");
-                text = textToHtml(text, artistName);
-                DataBase.saveArtist(dataBase, artistName, text);
+            .build()
+        val NYTimesAPI = retrofit.create(NYTimesAPI::class.java)
+        Log.e("TAG", "artistName $artistName")
+        Thread {
+            var text = DataBase.getInfo(dataBase, artistName)
+            if (text != null) { // exists in db
+                text = "[*]$text"
+            } else { // get from service
+                val callResponse: Response<String>
+                try {
+                    callResponse = NYTimesAPI.getArtistInfo(artistName).execute()
+                    Log.e("TAG", "JSON " + callResponse.body())
+                    val gson = Gson()
+                    val jobj = gson.fromJson(callResponse.body(), JsonObject::class.java)
+                    val response = jobj["response"].asJsonObject
+                    val _abstract = response["docs"].asJsonArray[0].asJsonObject["abstract"]
+                    val url = response["docs"].asJsonArray[0].asJsonObject["web_url"]
+                    if (_abstract == null) {
+                        text = "No Results"
+                    } else {
+                        text = _abstract.asString.replace("\\n", "\n")
+                        text = textToHtml(text, artistName)
+                        DataBase.saveArtist(dataBase, artistName, text)
+                    }
+                    val urlString = url.asString
+                    findViewById<View>(R.id.openUrlButton).setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(urlString)
+                        startActivity(intent)
+                    }
+                } catch (e1: IOException) {
+                    Log.e("TAG", "Error $e1")
+                    e1.printStackTrace()
+                }
             }
+            val imageUrl =
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
+            Log.e("TAG", "Get Image from $imageUrl")
+            val finalText = text
+            runOnUiThread {
+                Picasso.get().load(imageUrl).into(findViewById<View>(R.id.imageView) as ImageView)
+                textMoreDetails!!.text = Html.fromHtml(finalText)
+            }
+        }.start()
+    }
 
+    private var dataBase: DataBase? = null
+    private fun open(artist: String?) {
+        dataBase = DataBase(this)
+        DataBase.saveArtist(dataBase, "test", "sarasa")
+        Log.e("TAG", "" + DataBase.getInfo(dataBase, "test"))
+        Log.e("TAG", "" + DataBase.getInfo(dataBase, "nada"))
+        getArtistInfo(artist)
+    }
 
-            final String urlString = url.getAsString();
-            findViewById(R.id.openUrlButton).setOnClickListener(new View.OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse(urlString));
-                startActivity(intent);
-              }
-            });
-
-          } catch (IOException e1) {
-            Log.e("TAG", "Error " + e1);
-            e1.printStackTrace();
-          }
+    companion object {
+        const val ARTIST_NAME_EXTRA = "artistName"
+        fun textToHtml(text: String, term: String?): String {
+            val builder = StringBuilder()
+            builder.append("<html><div width=400>")
+            builder.append("<font face=\"arial\">")
+            val textWithBold = text
+                .replace("'", " ")
+                .replace("\n", "<br>")
+                .replace(
+                    "(?i)$term".toRegex(),
+                    "<b>" + term!!.uppercase(Locale.getDefault()) + "</b>"
+                )
+            builder.append(textWithBold)
+            builder.append("</font></div></html>")
+            return builder.toString()
         }
-        String imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU";
-        Log.e("TAG","Get Image from " + imageUrl);
-
-        final String finalText = text;
-        runOnUiThread( () -> {
-          Picasso.get().load(imageUrl).into((ImageView) findViewById(R.id.imageView));
-          textMoreDetails.setText(Html.fromHtml( finalText));
-        });
-      }
-    }).start();
-  }
-
-  private DataBase dataBase = null;
-
-  private void open(String artist) {
-    dataBase = new DataBase(this);
-    DataBase.saveArtist(dataBase, "test", "sarasa");
-    Log.e("TAG", ""+ DataBase.getInfo(dataBase,"test"));
-    Log.e("TAG",""+ DataBase.getInfo(dataBase,"nada"));
-    getArtistInfo(artist);
-  }
-
-  public static String textToHtml(String text, String term) {
-    StringBuilder builder = new StringBuilder();
-    builder.append("<html><div width=400>");
-    builder.append("<font face=\"arial\">");
-    String textWithBold = text
-            .replace("'", " ")
-            .replace("\n", "<br>")
-            .replaceAll("(?i)" + term, "<b>" + term.toUpperCase() + "</b>");
-    builder.append(textWithBold);
-    builder.append("</font></div></html>");
-    return builder.toString();
-  }
-
+    }
 }
