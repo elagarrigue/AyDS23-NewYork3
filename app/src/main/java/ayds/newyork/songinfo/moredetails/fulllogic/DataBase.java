21f11ca56a080968326a76a37b0dc1949fd374f8
nicolastradivarius <gonzaleznicolas574@gmail.com>
2023-04-24 00:02:42 -0300

    Deleted test function + minor changes

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.java
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.java
@@ -13,118 +13,71 @@ import java.util.List;
 
 public class DataBase extends SQLiteOpenHelper {
 
-
-
-  public static void saveArtist(DataBase dbHelper, String artist, String info)
-  {
-    SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-    ContentValues values = new ContentValues();
-    values.put("artist", artist);
-    values.put("info", info);
-    values.put("source", 1);
-
-    long newRowId = db.insert("artists", null, values);
-  }
-
-  public static String getInfo(DataBase dbHelper, String artist)
-  {
-
-    SQLiteDatabase db = dbHelper.getReadableDatabase();
-
-    String[] projection = {
-            "id",
-            "artist",
-            "info"
-    };
-
-    String selection = "artist  = ?";
-    String[] selectionArgs = { artist };
-
-    String sortOrder = "artist DESC";
-
-    Cursor cursor = db.query(
-            "artists",
-            projection,
-            selection,
-            selectionArgs,
-            null,
-            null,
-            sortOrder
-    );
-
-    List<String> items = new ArrayList<String>();
-    while(cursor.moveToNext()) {
-      String info = cursor.getString(
-              cursor.getColumnIndexOrThrow("info"));
-      items.add(info);
-    }
-    cursor.close();
-
-    if(items.isEmpty()) return null;
-    else return items.get(0);
-  }
-
-  public DataBase(Context context) {
-    super(context, "dictionary.db", null, 1);
-  }
-
-  @Override
-  public void onCreate(SQLiteDatabase db) {
-    db.execSQL(
-            "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)");
-
-    Log.i("DB", "DB created");
-  }
-
-  @Override
-  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-  }
-
-  public static void testDB()
-  {
-
-    Connection connection = null;
-    try
+    public static void saveArtist(DataBase dbHelper, String artist, String info)
     {
-      // create a database connection
-      connection = DriverManager.getConnection("jdbc:sqlite:./dictionary.db");
-      Statement statement = connection.createStatement();
-      statement.setQueryTimeout(30);  // set timeout to 30 sec.
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-      //statement.executeUpdate("drop table if exists person");
-      //statement.executeUpdate("create table person (id integer, name string)");
-      //statement.executeUpdate("insert into person values(1, 'leo')");
-      //statement.executeUpdate("insert into person values(2, 'yui')");
-      ResultSet rs = statement.executeQuery("select * from artists");
-      while(rs.next())
-      {
-        // read the result set
-        System.out.println("id = " + rs.getInt("id"));
-        System.out.println("artist = " + rs.getString("artist"));
-        System.out.println("info = " + rs.getString("info"));
-        System.out.println("source = " + rs.getString("source"));
+        ContentValues values = new ContentValues();
+        values.put("artist", artist);
+        values.put("info", info);
+        values.put("source", 1);
 
-      }
+        long newRowId = db.insert("artists", null, values);
     }
-    catch(SQLException e)
+
+    public static String getInfo(DataBase dbHelper, String artist)
     {
-      // if the error message is "out of memory",
-      // it probably means no database file is found
-      System.err.println(e.getMessage());
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                "id",
+                "artist",
+                "info"
+        };
+
+        String selection = "artist  = ?";
+        String[] selectionArgs = { artist };
+
+        String sortOrder = "artist DESC";
+
+        Cursor cursor = db.query(
+                "artists",
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        List<String> items = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            String info = cursor.getString(
+                    cursor.getColumnIndexOrThrow("info"));
+            items.add(info);
+        }
+        cursor.close();
+
+        if(items.isEmpty()) return null;
+        else return items.get(0);
     }
-    finally
-    {
-      try
-      {
-        if(connection != null)
-          connection.close();
-      }
-      catch(SQLException e)
-      {
-        System.err.println(e);
-      }
+
+    public DataBase(Context context) {
+
+        super(context, "dictionary.db", null, 1);
     }
-  }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "create table artists (id INTEGER PRIMARY KEY AUTOINCREMENT, artist string, info string, source integer)");
+
+        Log.i("DB", "DB created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
