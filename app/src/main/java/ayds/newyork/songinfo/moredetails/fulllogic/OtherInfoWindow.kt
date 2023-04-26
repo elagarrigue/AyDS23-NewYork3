f55ca698aecb4697d652340ac7fd1edeba41637e
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-04-26 00:14:42 -0300

    Refactor listo

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -11,9 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import ayds.newyork.songinfo.R
 import ayds.newyork.songinfo.utils.UtilsInjector
 import ayds.newyork.songinfo.utils.view.ImageLoader
-import com.google.gson.Gson
-import com.google.gson.JsonArray
-import com.google.gson.JsonObject
+import com.google.gson.*
+import com.google.gson.JsonPrimitive
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -55,30 +54,47 @@ class OtherInfoWindow : AppCompatActivity() {
 
     private fun getArtistInfo(artistName: String?) {
         Thread {
-            var text = DataBase.getInfo(dataBase, artistName)
-            val isInDataBase = text != null
-            if (isInDataBase) {
-                text = "[*]$text"
-            } else {
-                val docs = getInfoFromDataBase(artistName)
+            var text = getInfoFromDataBase(artistName)
+            if (text == null) {
+                val docs = getInfoFromAPI(artistName)
                 val abstract = docs?.get(0)?.asJsonObject?.get(ABSTRACT)
                 val url = docs?.get(0)?.asJsonObject?.get(WEB_URL)
-                if (abstract == null) {
-                    text = "No Results"
-                } else {
-                    text = abstract.asString.replace("\\n", "\n")
-                    text = textToHtml(text, artistName)
+                if (abstract != null) {
+                    text = getTextFromAbstract(abstract as JsonPrimitive, artistName)
                     DataBase.saveArtist(dataBase, artistName, text)
                 }
-                if (url != null) {
+                if (url != null)
                     initListeners(url.asString)
-                }
             }
             updateMoreDetailsText(text)
         }.start()
     }
 
-    private fun getInfoFromDataBase(artistName: String?): JsonArray? {
+    private fun getTextFromAbstract(abstract : JsonPrimitive, artistName: String?) : String?{
+        var text = "No Results"
+        if (abstract != null) {
+            text = abstract.asString.replace("\\n", "\n")
+            text = textWithBold(text, artistName)
+            text = textToHtml(text)
+        }
+        return text
+    }
+
+    private fun textWithBold(text: String, artistName: String?): String {
+        val textWithSpaces = text.replace("'", " ")
+        val textWithJumplines = textWithSpaces.replace("\n", "<br>")
+        val termUpperCase = artistName?.uppercase(Locale.getDefault())
+        return textWithJumplines.replace("(?i)$artistName".toRegex(), "<b>$termUpperCase</b>")
+    }
+
+    private fun getInfoFromDataBase(artistName: String?) : String? {
+        var text: String? = DataBase.getInfo(dataBase, artistName)
+        if (text != null)
+            text = "[*]$text"
+        return text
+    }
+
+    private fun getInfoFromAPI(artistName: String?): JsonArray? {
         return try {
             val callResponse: Response<String> = newYorkTimesAPI.getArtistInfo(artistName).execute()
             val jobj = Gson().fromJson(callResponse.body(), JsonObject::class.java)
@@ -103,26 +119,19 @@ class OtherInfoWindow : AppCompatActivity() {
         }
     }
 
-    private fun updateMoreDetailsText(text: String) {
+    private fun updateMoreDetailsText(text: String?) {
         runOnUiThread {
             moreDetailsTextView.text = Html.fromHtml(text)
         }
     }
 
-    private fun textToHtml(text: String, term: String?): String {
-        val textWithBold = text
-            .replace("'", " ")
-            .replace("\n", "<br>")
-            .replace(
-                term!!.toRegex(RegexOption.IGNORE_CASE),
-                "<b>" + term.uppercase(Locale.getDefault()) + "</b>"
-            )
-        return StringBuilder()
-            .append("<html><div width=400>")
-            .append("<font face=\"arial\">")
-            .append(textWithBold)
-            .append("</font></div></html>")
-            .toString()
+    private fun textToHtml(text: String): String {
+        val builder = StringBuilder()
+        builder.append("<html><div width=400>")
+        builder.append("<font face=\"arial\">")
+        builder.append(text)
+        builder.append("</font></div></html>")
+        return builder.toString()
     }
 
     companion object {
