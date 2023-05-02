4a880d4977ad72a5f7fcab624cccf2f76a19e746
Nicolás <gonzaleznicolas114@outlook.com>
2023-05-01 22:06:36 -0300

    Added URL column to database

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/DataBase.kt
@@ -9,13 +9,14 @@ import android.database.sqlite.SQLiteOpenHelper
 private const val TABLE_NAME = "artists"
 private const val COLUMN_ID = "id"
 private const val COLUMN_ARTIST = "artist"
-private const val COLUMN_SOURCE = "source"
 private const val COLUMN_INFO = "info"
+private const val COLUMN_SOURCE = "source"
+private const val COLUMN_URL = "url"
 private const val NYTIMES_SOURCE = 1
 private const val SELECTION_FILTER = "$COLUMN_ARTIST = ?"
 private const val SELECTION_ORDER = "$COLUMN_ARTIST desc"
 private const val DATABASE_NAME = "dictionary.db"
-private const val DATABASE_CREATION_QUERY = "create table $TABLE_NAME ($COLUMN_ID integer PRIMARY KEY AUTOINCREMENT, $COLUMN_ARTIST string, $COLUMN_INFO string, $COLUMN_SOURCE integer)"
+private const val DATABASE_CREATION_QUERY = "create table $TABLE_NAME ($COLUMN_ID integer PRIMARY KEY AUTOINCREMENT, $COLUMN_ARTIST string, $COLUMN_INFO string, $COLUMN_SOURCE integer, $COLUMN_URL string)"
 
 class DataBase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
 
@@ -24,6 +25,7 @@ class DataBase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null
             put(COLUMN_ARTIST, artistInfo.artist)
             put(COLUMN_INFO, artistInfo.abstract)
             put(COLUMN_SOURCE, NYTIMES_SOURCE)
+            put(COLUMN_URL, artistInfo.url)
         }
         this.writableDatabase.insert(TABLE_NAME, null, values)
     }
