1c821ceea2dac9c442bfdf2b80772e8ba270061b
AgusF22 <agus_p3@hotmail.com>
2023-05-09 17:03:05 -0300

    added methods to NYTimesArtistInfoLocalStorage

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/data/local/nytimes/NYTimesArtistInfoLocalStorage.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/data/local/nytimes/NYTimesArtistInfoLocalStorage.kt
@@ -1,4 +1,14 @@
 package ayds.newyork.songinfo.moredetails.model.data.local.nytimes
 
+import ayds.newyork.songinfo.moredetails.model.domain.entities.ArtistInfo
+
 interface NYTimesArtistInfoLocalStorage {
+
+    fun updateInfoByName(query: String, artistId: String)
+
+    fun insertInfo(query: String, info: ArtistInfo)
+
+    fun getInfoByName(name: String): ArtistInfo?
+
+    fun getInfoById(id: String): ArtistInfo?
 }
