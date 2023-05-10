acc94df5e7e5f2c93faa9ddb8abae915945d40e9
Erik Kovach <78232947+CoachVach@users.noreply.github.com>
2023-05-09 21:22:05 -0300

    Service and Resolver parcial Implementation

--- app/src/main/java/ayds/newyork/songinfo/moredetails/data/external/nytimes/artistinfo/NYTimesArtistInfoServiceImpl.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/data/external/nytimes/artistinfo/NYTimesArtistInfoServiceImpl.kt
@@ -4,9 +4,17 @@ import ayds.newyork.songinfo.home.model.entities.Song
 import ayds.newyork.songinfo.moredetails.data.external.nytimes.NYTimesArtistInfoService
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo
 
-class NYTimesArtistInfoServiceImpl: NYTimesArtistInfoService {
+class NYTimesArtistInfoServiceImpl(
+    private val nyTimesAPI: NYTimesAPI,
+    private val nyTimesToArtistInfoResolver: NYTimesToArtistInfoResolver,
+): NYTimesArtistInfoService {
 
-    override fun getArtistInfo(artist: String): ArtistInfo? {
-        TODO("Not yet implemented")
+    override fun getArtistInfo(artist: String): ArtistInfo?{
+        val callResponse = getArtistInfoFromService(artist)
+        val info = nyTimesToArtistInfoResolver.updateInfoArtist(callResponse, artist);
+        val url = "" //TODO : Generar url
+        return ArtistInfo(artist,info,url,false);
     }
+
+    private fun getArtistInfoFromService(artistName: String?) = nyTimesAPI.getArtistInfo(artistName).execute()
 }
--- app/src/main/java/ayds/newyork/songinfo/moredetails/data/external/nytimes/artistinfo/NYTimesToArtistInfoResolver.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/data/external/nytimes/artistinfo/NYTimesToArtistInfoResolver.kt
@@ -1,2 +1,29 @@
 package ayds.newyork.songinfo.moredetails.data.external.nytimes.artistinfo
 
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import retrofit2.Response
+
+private const val P_RESPONSE = "response"
+private const val NO_RESULTS = "No Results"
+interface NYTimesToArtistInfoResolver{
+    fun updateInfoArtist(callResponse: Response<String>,nameArtist: String?): String
+}
+
+internal class JsonToArtistInfoResolver : NYTimesToArtistInfoResolver{
+    override fun updateInfoArtist(callResponse: Response<String>, nameArtist: String?): String {
+        val response = getJson(callResponse)[P_RESPONSE].asJsonObject
+        val abstract = getAsJsonObject(response)
+        return abstract?.asString?.replace("\\n", "\n") ?: NO_RESULTS
+    }
+    private fun getJson(callResponse: Response<String>): JsonObject {
+        val gson = Gson()
+        return gson.fromJson(callResponse.body(), JsonObject::class.java)
+    }
+
+    private fun getAsJsonObject(response: JsonObject): JsonElement? {
+        return response["docs"].asJsonArray[0].asJsonObject["abstract"]
+    }
+
+}
