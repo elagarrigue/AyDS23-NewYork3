2c8a964ad787cf3126897f7b3884c2d47a4fb64a
AgusF22 <agus_p3@hotmail.com>
2023-05-21 14:00:52 -0300

    Modified presenter tests

--- app/src/test/java/ayds/newyork/songinfo/moreDetails/presentation/presenter/MoreDetailsPresenterTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moreDetails/presentation/presenter/MoreDetailsPresenterTest.kt
@@ -1,24 +1,38 @@
 package ayds.newyork.songinfo.moreDetails.presentation.presenter
+
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo.NYTArtistInfo
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo.EmptyArtistInfo
 import ayds.newyork.songinfo.moredetails.domain.repository.ArtistRepository
 import ayds.newyork.songinfo.moredetails.presentation.presenter.ArtistAbstractHelper
 import ayds.newyork.songinfo.moredetails.presentation.presenter.MoreDetailsPresenterImpl
 import ayds.newyork.songinfo.moredetails.presentation.presenter.MoreDetailsUIState
-import ayds.observer.Subject
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Test
+
 class MoreDetailsPresenterTest {
     private val artistRepository: ArtistRepository = mockk()
     private val artistAbstractHelper: ArtistAbstractHelper = mockk()
 
-    private lateinit var uiState: MoreDetailsUIState
-    private val onActionSubject: Subject<MoreDetailsUIState> = Subject()
-
     private val presenter by lazy {
         MoreDetailsPresenterImpl(artistRepository,artistAbstractHelper)
+    }
+
+    @Test
+    fun `on getArtistInfo it should notify the update`() {
+        val artistInfo = NYTArtistInfo("artist","abstract", "url")
+        val presenterTester: (MoreDetailsUIState) -> Unit = mockk(relaxed = true)
+        every { artistRepository.searchArtistInfo("artist") } returns artistInfo
+        every { artistAbstractHelper.getInfo(artistInfo) } returns "abstract"
+
+        presenter.uiStateObservable.subscribe {
+            presenterTester(it)
+        }
+        presenter.getArtistInfo("artist")
+
+        verify { presenterTester(presenter.uiState) }
     }
 
     @Test
@@ -34,7 +48,6 @@ class MoreDetailsPresenterTest {
         Thread.sleep(1000)
         assertEquals(expectedUiState, presenter.uiState)
     }
-
 
     @Test
     fun `getArtistInfo should update UI state as empty when EmptyArtistInfo`() {
