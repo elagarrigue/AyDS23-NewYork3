c1160554b9f80d3ee8176d3b02cc4b0e92fc8544
Erik Kovach <78232947+CoachVach@users.noreply.github.com>
2023-05-24 16:08:05 -0300

    ArtistInfoBroker.kt implemented

--- app/src/main/java/ayds/newyork/songinfo/moredetails/data/external/ArtistInfoBroker.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/data/external/ArtistInfoBroker.kt
@@ -1,7 +1,24 @@
 package ayds.newyork.songinfo.moredetails.data.external
 
-interface ArtistInfoBroker {
+import ayds.newyork.songinfo.moredetails.domain.entities.Card
 
+interface ArtistInfoBroker {
+    fun getArtistInfo(artist:String) :List<Card?>
 }
-class ArtistInfoBrokerImpl: ArtistInfoBroker {
+
+class ArtistInfoBrokerImpl(
+    private val proxyLisboa: LisboaProxy,
+    private val proxyNewYorkTimes: NewYorkTimesProxy,
+    private val proxyWinchester:WinchesterProxy
+): ArtistInfoBroker {
+
+    override fun getArtistInfo(artist: String): List<Card?> {
+        val cardList: MutableList<Card?> = mutableListOf()
+        cardList.add(proxyLisboa.getArtistInfo(artist))
+        cardList.add(proxyNewYorkTimes.getArtistInfo(artist))
+        cardList.add(proxyWinchester.getArtistInfo(artist))
+
+        return cardList
+    }
+
 }
