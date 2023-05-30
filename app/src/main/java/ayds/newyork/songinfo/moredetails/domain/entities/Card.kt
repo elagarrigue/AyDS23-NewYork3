a7a1d82c3b36cc9ecbc493223c9b8feb6f5ea588
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-30 11:10:08 -0300

    Actualizacion Card y MoreDetailsUIState

--- app/src/main/java/ayds/newyork/songinfo/moredetails/domain/entities/Card.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/domain/entities/Card.kt
@@ -1,9 +1,15 @@
 package ayds.newyork.songinfo.moredetails.domain.entities
 
 data class Card(
-    val description: String,
+    var description: String,
     val infoUrl: String,
-    val source: String,
-    val sourceLogoUrl: String,
+    val source: Source,
+    val sourceLogoUrl: String = "https://forum.hestiacp.com/uploads/default/optimized/2X/4/4d6f29ac652c55cde2faf6026c9852214fb74196_2_690x471.png",
     var isLocallyStored: Boolean = false
 )
+
+enum class Source{
+    NYTimes,
+    Wikipedia,
+    LastFM
+}
--- app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsUIState.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsUIState.kt
@@ -1,5 +1,13 @@
 package ayds.newyork.songinfo.moredetails.presentation.presenter
 
+import ayds.newyork.songinfo.moredetails.domain.entities.Card
+
+data class OtherInfoUiState(
+    val nYTimesCard: Card,
+    val wikipediaCard: Card,
+    val lastFMCard: Card
+)
+/*
 data class MoreDetailsUIState(
     val abstract: String = "",
     val url: String = "",
@@ -11,3 +19,4 @@ data class MoreDetailsUIState(
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
     }
 }
+*/
