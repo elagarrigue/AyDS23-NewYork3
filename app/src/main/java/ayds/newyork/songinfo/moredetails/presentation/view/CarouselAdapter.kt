14b2a835ee062adf93aa771e6708ff3c209736b3
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-30 13:10:46 -0300

    CarouselAdapter terminado

--- app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/view/CarouselAdapter.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/view/CarouselAdapter.kt
@@ -1,10 +1,23 @@
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ayds.newyork.songinfo.R
+import ayds.newyork.songinfo.moredetails.domain.entities.Card
+import ayds.newyork.songinfo.moredetails.domain.entities.Source
+import ayds.newyork.songinfo.moredetails.domain.entities.Source.*
+import ayds.newyork.songinfo.moredetails.presentation.view.MoreDetailsView
+import ayds.newyork.songinfo.utils.UtilsInjector
+import com.squareup.picasso.Picasso
 
-class CarouselAdapter : RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>() {
+class CarouselAdapter(
+    private val cards: List<Card>,
+    private val moreDetailsView: MoreDetailsView
+): RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.carousel_item, parent, false)
@@ -12,15 +25,40 @@ class CarouselAdapter : RecyclerView.Adapter<CarouselAdapter.CarouselViewHolder>
     }
 
     override fun onBindViewHolder(holder: CarouselViewHolder, position: Int) {
-        // Aquí puedes realizar la lógica para mostrar los datos en cada elemento del carrusel
+        val card = cards[position]
+        setImageView(holder.imageView, card.sourceLogoUrl)
+        setTextView(holder.textSource, card.source.name)
+        setTextViewHtml(holder.textMoreDetails, card.description)
+        updateListenerUrl(holder.urlButton, card.infoUrl)
+    }
+
+    private fun setImageView(imageView: ImageView, image: String) {
+        val picasso =  Picasso.get()
+        val requestCreator = picasso.load(image)
+        requestCreator.into(imageView)
+    }
+
+    private fun updateListenerUrl(urlButton: Button, infoUrl: String) {
+        urlButton.setOnClickListener { UtilsInjector.navigationUtils.openExternalUrl(moreDetailsView, infoUrl) }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun setTextViewHtml(textView: TextView, text: String) {
+        textView.text = Html.fromHtml(text)
+    }
+
+    private fun setTextView(textView: TextView, text: String) {
+        textView.text = text
     }
 
     override fun getItemCount(): Int {
-        // Retorna la cantidad de elementos en el carrusel
-        return 0
+        return cards.size
     }
 
     inner class CarouselViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // Realiza las referencias a los elementos de "carousel_item.xml" aquí si necesitas hacer alguna interacción
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        val textSource: TextView = itemView.findViewById(R.id.textSource)
+        val textMoreDetails: TextView = itemView.findViewById(R.id.textMoreDetails)
+        val urlButton: Button = itemView.findViewById(R.id.openUrlButton)
     }
 }
