a2ed8d7023a71f09e21f3703028e9a0e837b8550
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-30 18:05:09 -0300

    Correccion MoreDetailsUIState

--- app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsUIState.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsUIState.kt
@@ -2,7 +2,7 @@ package ayds.newyork.songinfo.moredetails.presentation.presenter
 
 import ayds.newyork.songinfo.moredetails.domain.entities.Card
 
-data class OtherInfoUiState(
+data class MoreDetailsUIState(
     val nYTimesCard: Card,
     val wikipediaCard: Card,
     val lastFMCard: Card
