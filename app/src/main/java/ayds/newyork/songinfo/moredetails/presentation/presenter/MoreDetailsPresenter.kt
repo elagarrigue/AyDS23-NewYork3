e8159dd036f8fbc32fcf516b44d6c4c09e42f444
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-30 20:52:04 -0300

    Actualizacion Presenter

--- app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsPresenter.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsPresenter.kt
@@ -3,7 +3,9 @@ package ayds.newyork.songinfo.moredetails.presentation.presenter
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo.NYTArtistInfo
 import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo.EmptyArtistInfo
-import ayds.newyork.songinfo.moredetails.domain.repository.ArtistRepository
+import ayds.newyork.songinfo.moredetails.domain.entities.Card
+import ayds.newyork.songinfo.moredetails.domain.entities.Source
+import ayds.newyork.songinfo.moredetails.domain.repository.CardRepository
 import ayds.observer.Observable
 import ayds.observer.Subject
 
@@ -15,18 +17,18 @@ interface MoreDetailsPresenter {
 }
 
 internal class MoreDetailsPresenterImpl(
-    var artistInfoRepository: ArtistRepository,
-    var artistAbstractHelper: ArtistAbstractHelper
+    var cardRepository: CardRepository,
+    var artistAbstractHelper: ArtistAbstractHelper,
+    override var uiState: MoreDetailsUIState
 ): MoreDetailsPresenter {
 
     private val onActionSubject = Subject<MoreDetailsUIState>()
     override var uiStateObservable = onActionSubject
-    override var uiState = MoreDetailsUIState()
 
     override fun getArtistInfo(artistName: String) {
         Thread {
-            val artistInfo = artistInfoRepository.searchArtistInfo(artistName)
-            updateUiState(artistInfo)
+            //val cards = cardRepository.searchArtistInfo(artistName)
+            updateUiState(artistName)
             notifyUpdate(uiState)
         }.start()
     }
@@ -35,13 +37,49 @@ internal class MoreDetailsPresenterImpl(
         uiStateObservable.notify(uiState)
     }
 
-    private fun updateUiState(artistInfo: ArtistInfo) {
-        when (artistInfo) {
-            is NYTArtistInfo -> updateArtistInfoUiState(artistInfo)
-            EmptyArtistInfo -> updateEmptyUiState()
-        }
+    private fun updateUiState(artistName: String) {
+        val cards = ArrayList<Card>()
+        cards.add(
+            Card("textPrueba1", "https://www.google.com.ar/",
+                Source.NYTimes, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU", true)
+        )
+        cards.add(
+            Card("textPrueba2", "https://www.facebook.com/",
+                Source.Wikipedia, "https://upload.wikimedia.org/wikipedia/commons/8/8c/Wikipedia-logo-v2-es.png", true)
+        )
+        cards.add(
+            Card("textPrueba3", "https://www.twitter.com/",
+                Source.LastFM, "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png", true)
+        )
+        uiState = getUiState(cards, artistName)
+        notifyUpdate(uiState)
     }
 
+    private fun getNYTimesCard(cards: List<Card>): Card {
+        val newYorkTimesCard = cards.find { it.source == Source.NYTimes }
+        return newYorkTimesCard ?: Card(source = Source.NYTimes)
+    }
+
+    private fun getWikipediaCard(cards: List<Card>): Card {
+        val wikipediaCard = cards.find { it.source == Source.Wikipedia }
+        return wikipediaCard ?: Card(source = Source.Wikipedia)
+    }
+
+    private fun getLastFMCard(cards: List<Card>): Card {
+        val lastFMCard = cards.find { it.source == Source.LastFM }
+        return lastFMCard ?: Card(source = Source.LastFM)
+    }
+
+    private fun getUiState(cards: List<Card>, artistName: String): MoreDetailsUIState{
+        val newYorkTimesCard = getNYTimesCard(cards)
+        artistAbstractHelper.getInfo(newYorkTimesCard)
+        val wikipediaCard = getWikipediaCard(cards)
+        artistAbstractHelper.getInfo(wikipediaCard)
+        val lastFMCard = getLastFMCard(cards)
+        artistAbstractHelper.getInfo(lastFMCard)
+        return MoreDetailsUIState(newYorkTimesCard, wikipediaCard, lastFMCard)
+    }
+/*
     private fun updateArtistInfoUiState(artistInfo: NYTArtistInfo) {
         uiState = uiState.copy(
             abstract = artistAbstractHelper.getInfo(artistInfo),
@@ -57,4 +95,6 @@ internal class MoreDetailsPresenterImpl(
             actionsEnabled = false
         )
     }
+    */
+
 }
