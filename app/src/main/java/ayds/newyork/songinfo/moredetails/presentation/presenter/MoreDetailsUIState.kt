6f718cf547102aa6e4376b88f30f0d46ec2c063a
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-31 11:36:17 -0300

    Modificado UIState

--- app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsUIState.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/presentation/presenter/MoreDetailsUIState.kt
@@ -3,14 +3,5 @@ package ayds.newyork.songinfo.moredetails.presentation.presenter
 import ayds.newyork.songinfo.moredetails.domain.entities.Card
 
 data class MoreDetailsUIState(
-    val nYTimesCard: Card,
-    val wikipediaCard: Card,
-    val lastFMCard: Card
+    var cardList: List<Card>
 )
-/*
-    companion object {
-        const val TITLE_IMAGE_URL =
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
-    }
-}
-*/
