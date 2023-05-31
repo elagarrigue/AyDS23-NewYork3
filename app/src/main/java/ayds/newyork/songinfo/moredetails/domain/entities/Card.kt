22820c1631aba1404b91a3ff9d238d504801c231
Danilo La Colla <54331718+DaniloLaColla@users.noreply.github.com>
2023-05-31 10:39:47 -0300

    Update Card.kt

--- app/src/main/java/ayds/newyork/songinfo/moredetails/domain/entities/Card.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/domain/entities/Card.kt
@@ -2,6 +2,7 @@ package ayds.newyork.songinfo.moredetails.domain.entities
 
 data class Card(
     var description: String = "",
+    var artistName: String = "",
     var infoUrl: String = "",
     var source: Source,
     var sourceLogoUrl: String = "",
