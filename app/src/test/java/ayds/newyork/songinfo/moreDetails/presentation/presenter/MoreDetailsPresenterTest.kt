74f02f23d0f9d1e96cb1f04c99b4c61cda4e39ed
Erik Kovach <78232947+CoachVach@users.noreply.github.com>
2023-05-31 22:16:41 -0300

    Corrected MoreDetailsPresenterTest

--- app/src/test/java/ayds/newyork/songinfo/moreDetails/presentation/presenter/MoreDetailsPresenterTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moreDetails/presentation/presenter/MoreDetailsPresenterTest.kt
@@ -1,6 +1,6 @@
 package ayds.newyork.songinfo.moreDetails.presentation.presenter
-import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo.NYTArtistInfo
-import ayds.newyork.songinfo.moredetails.domain.entities.ArtistInfo.EmptyArtistInfo
+import ayds.newyork.songinfo.moredetails.domain.entities.Card
+import ayds.newyork.songinfo.moredetails.domain.entities.Source
 import ayds.newyork.songinfo.moredetails.domain.repository.CardRepository
 import ayds.newyork.songinfo.moredetails.presentation.presenter.ArtistAbstractHelper
 import ayds.newyork.songinfo.moredetails.presentation.presenter.MoreDetailsPresenterImpl
@@ -18,16 +18,17 @@ class MoreDetailsPresenterTest {
     private val onActionSubject: Subject<MoreDetailsUIState> = Subject()
 
     private val presenter by lazy {
-        MoreDetailsPresenterImpl(cardRepository, artistAbstractHelper)
+        MoreDetailsPresenterImpl(cardRepository, artistAbstractHelper,uiState)
     }
 
     @Test
     fun `getArtistInfo should update UI state with artist info`() {
-        val artistInfo = NYTArtistInfo("artist", "abstract", "url")
-        val expectedUiState = MoreDetailsUIState("abstract", "url", true)
+        val card = Card("description", "artistName", "infoUrl", Source.NYTimes, "sourceLogoUrl")
+        val cardList = listOf(card)
+        val expectedUiState = MoreDetailsUIState(cardList)
 
-        every { cardRepository.searchArtistInfo("artist") } returns artistInfo
-        every { artistAbstractHelper.getInfo(artistInfo) } returns "abstract"
+        every { cardRepository.searchArtistInfo("artist") } returns cardList
+        every { artistAbstractHelper.getInfo(card) } returns "abstract"
 
         presenter.getArtistInfo("artist")
 
@@ -37,12 +38,13 @@ class MoreDetailsPresenterTest {
 
 
     @Test
-    fun `getArtistInfo should update UI state as empty when EmptyArtistInfo`() {
-        val artistInfo = EmptyArtistInfo
-        val expectedUiState = MoreDetailsUIState("", "", false)
+    fun `getArtistInfo should update UI state as empty when emptylist`() {
+        val card = Card("description", "artistName", "infoUrl", Source.NYTimes, "sourceLogoUrl")
+        val cardList = emptyList<Card>()
+        val expectedUiState = MoreDetailsUIState(cardList)
 
-        every { cardRepository.searchArtistInfo("artist") } returns artistInfo
-        every { artistAbstractHelper.getInfo(artistInfo) } returns ""
+        every { cardRepository.searchArtistInfo("artist") } returns cardList
+        every { artistAbstractHelper.getInfo(card) } returns ""
 
         presenter.getArtistInfo("artist")
         Thread.sleep(1000)
